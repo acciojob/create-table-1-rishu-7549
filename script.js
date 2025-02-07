76c103c2f54f728ec0d2b9390cd42f05b8dcafc3
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-07 12:43:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ function insert_Row() {
     //Write your code here
     let table = document.getElementById("sampleTable");
 	
-	 // let newRow = table.insertRow(0);
+	 let newRow = table.insertRow(0);
     
-  //   // Insert new cells in the new row
-  //   let cell1 = newRow.insertCell(0);
-  //   let cell2 = newRow.insertCell(1);
+    // Insert new cells in the new row
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
     
-  //   // Set the text content for each cell
-  //   cell1.textContent = 'New Cell1';
-  //   cell2.textContent = 'New Cell2';
+    // Set the text content for each cell
+    cell1.textContent = 'New Cell1';
+    cell2.textContent = 'New Cell2';
 
-	let newRow = document.createElement("tr");
-	let cell1 = document.createElement("td");
-	cell1.textContent = "New Cell1";
-	let cell2 = document.createElement("td");
-	cell2.textContent = "New Cell2";
+	// let newRow = document.createElement("tr");
+	// let cell1 = document.createElement("td");
+	// cell1.textContent = "New Cell1";
+	// let cell2 = document.createElement("td");
+	// cell2.textContent = "New Cell2";
 
-	newRow.appendChild(cell1);
-	newRow.appendChild(cell2);
+	// newRow.appendChild(cell1);
+	// newRow.appendChild(cell2);
 
-	table.insertBefore(newRow, table.firstChild);
+	// table.insertBefore(newRow, table.firstChild);
 	
   
 }
